ffa1bc0a11fbf51415d19fc9b9464bc921258142
Kaplaugher <kaplaugher94@gmail.com>
2017-08-23 14:07:52 -0400

    added react

--- routes/api.js
+++ routes/api.js
@@ -4,21 +4,32 @@ const Ninja = require('../models/ninja');
 
 // get a list of ninjas from database
 router.get('/ninjas', function(req, res, next) {
-  res.send({type: 'GET'});
+  Ninja.geoNear(
+    {type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
+  {maxDistance: 100000, spherical: true}
+  ).then(function(ninjas) {
+    res.send(ninjas)
+  });
 });
 // add a new ninja to the db
 router.post('/ninjas', function(req, res, next) {
   Ninja.create(req.body).then(function(ninja) {
     res.send(ninja);
-  });
+  }).catch(next);
 });
 // update a ninja in database
 router.put('/ninjas/:id', function(req, res, next) {
-  res.send({type: 'PUT'});
+  Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then(function() {
+    Ninja.findOne({_id: req.params.id}).then(function(ninja) {
+      res.send(ninja);
+    })
+  });
 });
 // delete ninja from database
-router.delete('/ninjas', function(req, res, next) {
-  res.send({type: 'DELETE'});
+router.delete('/ninjas/:id', function(req, res, next) {
+  Ninja.findByIdAndRemove({_id: req.params.id}).then(function(ninja) {
+    res.send(ninja);
+  });
 });
 
 module.exports = router;
